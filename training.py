4776bf78a919d9c05f568aaee85aaba28a01ae16
Animikh Aich <animikhaich@gmail.com>
2023-02-13 15:39:54 -0500

    Added: Resume Capability

--- training.py
+++ training.py
@@ -11,25 +11,29 @@ import time
 from tqdm import tqdm
 
 
-def train(data_folder, labels_path, save_path):
+def train(data_folder, labels_path, save_path, resume=False):
     """
     Function for training the network. You can make changes (e.g., add validation dataloader, change batch_size and #of epoch) accordingly.
     """
-    device_id = [0, 2]
+    device_id = [0]
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    if device == 'cuda':
-        infer_action = torch.nn.DataParallel(ClassificationNetwork(), device_ids=device_id).to(device)
-    else:
-        infer_action = ClassificationNetwork().to(device)
+    # if device == 'cuda':
+    #     infer_action = torch.nn.DataParallel(ClassificationNetwork(), device_ids=device_id).to(device)
+    # else:
+    infer_action = ClassificationNetwork().to(device)
     optimizer = torch.optim.Adam(infer_action.parameters(), lr=1e-2)
-
+    
+    model_name = "small_v1"
     nr_epochs = 100
     best_loss = 10e10
-    batch_size = 128 * 2
+    batch_size = 256
     nr_of_classes = 0  # needs to be changed
     start_time = time.time()
 
-    train_loader = get_dataloader(data_folder, labels_path, batch_size)
+    if resume:
+        infer_action.load_state_dict(torch.load(f"{model_name}_best.pt").state_dict())
+
+    train_loader = get_dataloader(data_folder, labels_path, batch_size, num_workers=20)
 
     for epoch in range(nr_epochs):
         total_loss = 0
@@ -47,14 +51,13 @@ def train(data_folder, labels_path, save_path):
             loss.backward()
             optimizer.step()
             total_loss += loss
-            
 
         time_per_epoch = (time.time() - start_time) / (epoch + 1)
         time_left = (1.0 * time_per_epoch) * (nr_epochs - 1 - epoch)
         print("Epoch %5d\t[Train]\tloss: %.6f \tETA: +%fs" % (
             epoch + 1, total_loss, time_left))
 
-        best_loss = save_model("small_v1", infer_action, total_loss, best_loss, mode="lowest")
+        best_loss = save_model(model_name, infer_action, total_loss, best_loss, mode="lowest")
 
 def save_model(name, model, val_metric, best_metric, mode='lowest'):
     torch.save(model, f'{name}_last.pt')
@@ -88,6 +91,7 @@ if __name__ == "__main__":
     parser.add_argument('-d', '--data_folder', default="./data/rgb", type=str, help='path to where you save the images you collect')
     parser.add_argument('-l', '--labels_path', default="./data/metrics/metrics.csv", type=str, help='path to where you save the labels you collect')
     parser.add_argument('-s', '--save_path', default="./", type=str, help='path where to save your model in .pth format')
+    parser.add_argument('-r', '--resume', default=False, help='Resume Training', action='store_true')
     args = parser.parse_args()
     
-    train(args.data_folder, args.labels_path, args.save_path)
+    train(args.data_folder, args.labels_path, args.save_path, resume=args.resume)
