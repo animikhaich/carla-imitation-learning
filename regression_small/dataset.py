33536866642c0a1fe2448967f6a42d4e3be5561d
Animikh Aich <animikhaich@gmail.com>
2023-02-18 20:10:09 -0500

    IterimUpdate: Added - Args, Early Stopping, Tensorboard Logging, Improved Resume, LR Scheduler Improved Logging, Improved Args, Improved DataParallel

--- regression_small/dataset.py
+++ regression_small/dataset.py
@@ -23,57 +23,9 @@ class CarlaDataset(Dataset):
                 transforms.Resize(image_size),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
             ])
-        
-        self.idx_to_action = {
-            0: "throttle",
-            1: "throttle_left",
-            2: "throttle_right",
-            3: "brake",
-            4: "brake_left",
-            5: "brake_right",
-            6: "OOD"
-        }
-
-        self.action_to_idx = {
-            "throttle": 0,
-            "throttle_left": 1,
-            "throttle_right": 2,
-            "brake": 3,
-            "brake_left": 4,
-            "brake_right": 5,
-            "OOD": 6
-        }
 
     def __len__(self):
-        return min(len(self.image_filenames), len(self.labels))
-
-    def actions_to_classes(self, throttle, steer, brake):
-        # Discretize Steering 
-        if steer < -self.delta:
-            steer = -1
-        elif steer > self.delta:
-            steer = 1
-        else:
-            steer = 0
-        
-        # Discretize Throttle
-        throttle = 1 if throttle > self.delta else 0
-
-        # Conditions
-        if brake and steer == 0:
-            return "brake"
-        elif brake and steer < 0:
-            return "brake_left"
-        elif brake and steer > 0:
-            return "brake_right"
-        elif throttle and steer == 0:
-            return "throttle"
-        elif throttle and steer < 0:
-            return "throttle_left"
-        elif throttle and steer > 0:
-            return "throttle_right"
-        else:
-            return "OOD"            
+        return min(len(self.image_filenames), len(self.labels))      
 
 
     def __getitem__(self, idx):
@@ -88,13 +40,14 @@ class CarlaDataset(Dataset):
         image = image.type(torch.float)
         image = self.transform(image) if self.transform else image
 
-        action = self.actions_to_classes(data.throttle, data.steer, data.brake)
-        label = torch.tensor(self.action_to_idx[action])
-        label = torch.nn.functional.one_hot(label, num_classes=len(self.action_to_idx))
+        # We use tanh activation for the steering and brake, so we need to normalize the data
+        throttle = (data.throttle - 0.5) * 2 # Range is now (-1, 1)
+        steer = data.steer # Range is already (-1, 1)
+        brake = (data.brake - 0.5) * 2 # Range is now (-1, 1)
+
+        label = torch.tensor([throttle, steer, brake])
 
         return (image, label.type(torch.DoubleTensor))
-
-
 
 
 def get_dataloader(data_dir, labels, batch_size, num_workers=32, shuffle=True):
--- regression_small/training.py
+++ regression_small/training.py
@@ -1,4 +1,5 @@
 import time
+import os
 import random
 import argparse
 
@@ -6,58 +7,100 @@ import torch
 
 from network import ClassificationNetwork
 from dataset import get_dataloader
+from torch.utils.tensorboard import SummaryWriter
+import datetime
 
 import time
 from tqdm import tqdm
 
+# TODO: Add Validation Dataloader and Validation Set
+# DONE: Early Stopping
+# TODO: Tensboard Dir in args
+# TODO: If dir does not exist, create it
+# TODO: Verify Model Saving Path and Model Loading Path
 
-def train(data_folder, labels_path, save_path, resume=False):
+def train(data_folder, labels_path, save_path, use_gpus=[0], load_model=None):
     """
     Function for training the network. You can make changes (e.g., add validation dataloader, change batch_size and #of epoch) accordingly.
     """
-    device_id = [0]
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    # if device == 'cuda':
-    #     infer_action = torch.nn.DataParallel(ClassificationNetwork(), device_ids=device_id).to(device)
-    # else:
-    infer_action = ClassificationNetwork().to(device)
-    optimizer = torch.optim.Adam(infer_action.parameters(), lr=1e-3)
+
+    # Choose Training Medium
+    if device == 'cuda' and len(args.use_gpus) > 1:
+        infer_action = torch.nn.DataParallel(ClassificationNetwork(), device_ids=args.use_gpus).to(device)
+        print("Using multiple GPUs: ", args.use_gpus)
+    elif device == 'cuda' and len(args.use_gpus) == 1:
+        infer_action = ClassificationNetwork().to(device)
+        print("Using single GPU: ", args.use_gpus[0])
+    else:
+        infer_action = ClassificationNetwork().to(device)
+        print("Using CPU")
     
-    model_name = "all_250k_small_v3"
-    nr_epochs = 300
+    # Define optimizer with learning rate scheduler
+    optimizer = torch.optim.Adam(infer_action.parameters(), lr=1e-3, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, min_lr=1e8, verbose=True)
+    
+    nr_epochs = 500
     best_loss = 10e10
     batch_size = 512
-    nr_of_classes = 0  # needs to be changed
     start_time = time.time()
 
-    if resume:
-        infer_action.load_state_dict(torch.load(f"{model_name}_best.pt").state_dict())
+    timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+    model_name = os.path.basename(save_path).split(".")[0]
+    writer = SummaryWriter(log_dir=os.path.join(args.tb_dir, f'{model_name}_{timestamp}'))
+    early_stopping = EarlyStopping(patience=10, mode='lowest') # Early Stopping if loss does not increase
+    
+    # Load Weights if specified
+    if load_model is not None and os.path.exists(load_model):
+        infer_action.load_state_dict(torch.load(load_model).state_dict())
+        print(f"Loaded Weights From: {load_model}")
 
-    train_loader = get_dataloader(data_folder, labels_path, batch_size, num_workers=16)
+    train_loader = get_dataloader(args.data_folder, args.labels_path, args.batch_size, num_workers=args.num_workers)
 
-    for epoch in range(nr_epochs):
+    for epoch in range(args.epochs):
         total_loss = 0
         batch_in = []
         batch_gt = []
 
-        for batch_idx, batch in tqdm(enumerate(train_loader), total=len(train_loader)):
+        for batch_idx, batch in tqdm(enumerate(train_loader), total=len(train_loader), disable=args.disable_tqdm):
             batch_in, batch_gt = batch[0].to(device), batch[1].to(device)
-            
             batch_out = infer_action(batch_in)
-
-            loss = cross_entropy_loss(batch_out, batch_gt)
+            loss = torch.nn.functional.mse_loss(batch_out, batch_gt)
             
-            optimizer.zero_grad()
+            scheduler.zero_grad()
             loss.backward()
-            optimizer.step()
+            scheduler.step()
             total_loss += loss
 
         time_per_epoch = (time.time() - start_time) / (epoch + 1)
-        time_left = (1.0 * time_per_epoch) * (nr_epochs - 1 - epoch)
-        print("Epoch %5d\t[Train]\tloss: %.6f \tETA: +%fs" % (
-            epoch + 1, total_loss, time_left))
+        time_left = (1.0 * time_per_epoch) * (args.epochs - 1 - epoch)
+        time_left = seconds_to_hms(time_left)
+        print("Epoch %5d\t[Train]\tloss: %.6f \tETA:" % (
+            epoch + 1, total_loss), time_left)
 
-        best_loss = save_model(model_name, infer_action, total_loss, best_loss, mode="lowest")
+        writer.add_scalar('Loss/train', total_loss, epoch)
+
+        best_loss = save_model(model_name, infer_action, total_loss, best_loss, mode="lowest") # Save Best and Last Model
+        if early_stopping(total_loss, best_loss, mode='lowest'): # Early Stopping if loss does not increase
+            print("Early Stopping")
+            break
+
+class EarlyStopping:
+    def __init__(self, patience=10, mode='lowest'):
+        self.patience = patience
+        self.counter = 0
+
+    def __call__(self, val_metric, best_metric, mode='lowest'):
+        self.counter += 1
+        if (mode == 'lowest' and val_metric <= best_metric and self.counter > self.patience) or \
+            (mode == 'highest' and val_metric >= best_metric and self.counter > self.patience):
+            print("Early Stopping after %d epochs" % self.counter)
+            return True
+
+def seconds_to_hms(seconds):
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return "%d:%02d:%02d" % (h, m, s)
 
 def save_model(name, model, val_metric, best_metric, mode='lowest'):
     torch.save(model, f'{name}_last.pt')
@@ -73,25 +116,13 @@ def save_model(name, model, val_metric, best_metric, mode='lowest'):
             return val_metric
     return best_metric
 
-def cross_entropy_loss(batch_out, batch_gt):
-    """
-    Calculates the cross entropy loss between the prediction of the network and
-    the ground truth class for one batch.
-                    C = number of classes
-    batch_out:      torch.Tensor of size (batch_size, C)
-    batch_gt:       torch.Tensor of size (batch_size, C)
-    return          float
-    """
-    loss = torch.nn.CrossEntropyLoss()
-    return loss(batch_out, batch_gt)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='EC500 Homework1 Imitation Learning')
-    parser.add_argument('-d', '--data_folder', default="./data/rgb", type=str, help='path to where you save the images you collect')
-    parser.add_argument('-l', '--labels_path', default="./data/metrics/output.csv", type=str, help='path to where you save the labels you collect')
-    parser.add_argument('-s', '--save_path', default="./", type=str, help='path where to save your model in .pth format')
-    parser.add_argument('-r', '--resume', default=False, help='Resume Training', action='store_true')
+    parser.add_argument('-d', '--data_folder', default="../data/rgb", type=str, help='path to where you save the images you collect')
+    parser.add_argument('-l', '--labels_path', default="../data/metrics/output.csv", type=str, help='path to where you save the labels you collect')
+    parser.add_argument('-m', '--model_path', default="../models/", type=str, help='path where to save your model in .pt format')
+    parser.add_argument('-p', '--load_model_path', default=None, help='path to load your model in .pt format')
     args = parser.parse_args()
     
-    train(args.data_folder, args.labels_path, args.save_path, resume=args.resume)
+    train(args.data_folder, args.labels_path, args.model_path, load_model=args.load_model_path)
